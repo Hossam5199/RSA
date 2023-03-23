07f0238740f8bb22084186a5f3804fafcf451401
Hossam5199 <59120544+Hossam5199@users.noreply.github.com>
2023-03-23 08:14:21 -0700

    Add files via upload

---
+++ src/rsa/Main.java
@@ -0,0 +1,18 @@
+package rsa;
+
+public class Main {
+
+/**
+ *
+ * @author hosso
+     * @param args
+ */
+
+	public static void main(String[] args) {
+		RSAFrame j = new RSAFrame();
+		j.setLocation(300, 300);
+		j.setVisible(true);
+
+	}
+
+}
